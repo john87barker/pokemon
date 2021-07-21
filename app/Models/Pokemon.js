0b634921cab4b03be3ed18f63a47e10d210492e4
john87barker <john87barker@gmail.com>
2021-07-21 15:12:48 -0600

    Controllers set up

--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -1,13 +1,16 @@
 export default class Value {
-    constructor(data) {
-        this.title = data.title
+    constructor({name, abilities, img, id}) {
+        this.name = name
+        this.abilities = abilities
+        this.img = img
+        this.id = id
     }
 
     get Template() {
 
         return /*html*/`
         <div class="card p-2 value">
-            ${this.title}
+            ${this.name}
         </div>
         `
     }
